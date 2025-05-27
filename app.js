0402e599913fcfcf61bc2c0b39639e321c7ca7e7
iAK01 <111150991+iAK01@users.noreply.github.com>
2025-05-27 19:23:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -681,11 +681,7 @@ class TripMaster {
 
 updatePackingComponents() {
     if (Object.keys(this.state.trip.items).length > 0) {
-        // Use loadItems method which exists in your ChecklistDisplay
         this.checklistDisplay.loadItems(this.state.trip.items, this.state.trip.completedItems);
-        
-        // Then call render with trip data for the summary
-        this.checklistDisplay.render(this.state.trip.items, this.state.trip);
     }
     
     if (this.state.trip.weather) {
